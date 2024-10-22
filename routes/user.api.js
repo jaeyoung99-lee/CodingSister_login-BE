6832544ee89fde82da0d04793bb295675536df8d
jy01.lee <jy01.lee@smarterbuild.net>
2024-10-23 02:50:53 +0900

    로그인 심화 BE

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userController = require("../controller/user.controller");
+const authController = require("../controller/auth.controller");
 const router = express.Router();
 
 // 1. 회원가입 endpoint
@@ -10,5 +11,8 @@ router.post("/", userController.createUser);
 // get을 사용하려면 url에 이메일과 패스워드를 넣어야 하는데 이러면 보안상 안되기 때문에
 // post를 사용하여 request body에 넣어서 전달하도록 함
 router.post("/login", userController.loginWithEmail);
+
+// 토큰을 통해 유저 id 빼내고 그 아이디로 유저 객체 찾아서 보내주기
+router.get("/me", authController.authenticate, userController.getUser);
 
 module.exports = router;
